a95561a3eeb533122c86dbc7ab99b3e226c04e35
Alexandra Knoblauch <114161001+Alexknob@users.noreply.github.com>
2023-02-13 11:33:38 +0000

    Add files via upload

---
+++ Coding Club/IntrotoPython/IntrotoPython.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 10:58:39 2023
+
+@author: Alexa
+"""
+
+#Intro to Python
+#Coding Club
+#https://ourcodingclub.github.io/tutorials/python-intro/
+
+#This script shows how to import and explore data in Python
+#and how to produce good scientific code
+
+# Tutorial Aims
+
+# Understand why Python is so useful for scientific programming
+# Installing Python and running a simple Python program
+# Reading data from a file
+# Get a feel for how Python looks and feels
+# Load data from a text file into memory and basic data structures
+# Moving beyond the core Python language with modules
+# A brief introduction to data analysis with the pandas package
+# Plotting data with Matplotlib
+
+
+# Understand why Python is so useful for scientific programming
+
+
+# simplicity of code language
+"Python".startswith("P")
+
+"thon" in "Python"
+
+print("xyz" in "Python")
+
+tastyword = "chocolate"
+x = 3
+
+if tastyword.count("o") < x and tastyword.endswith("e"):
+  print("You have won some tasty chocolate!")
+  
+
+# Installing Python and running a simple Python program
+
+print("Hello, World!")
+
+
+
+  
+  
